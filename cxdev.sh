b8a27fc91614206660764d338f7b3a72c0b9849a
Stefan Bechtold <bechte@users.noreply.github.com>
2024-12-25 02:44:33 +0100

    set correct script path when init script is sourced

--- cxdev.sh
+++ cxdev.sh
@@ -1,4 +1,10 @@
-export CXDEVHOME=$(dirname "$(realpath $0)")
+if [ -n "$BASH_VERSION" ]; then
+    CXDEVINITSCRIPT=$(realpath "$BASH_SOURCE")
+elif [ -n "$ZSH_VERSION" ]; then
+    CXDEVINITSCRIPT=$(realpath "${(%):-%N}")
+fi
+
+export CXDEVHOME=$(dirname "$CXDEVINITSCRIPT")
 export CXDEVSYNCDIR="$CXDEVHOME/dependencies/sapartefacts"
 
 # Include CX DEV environment files
